3a91e9bdb6d1e170775f3d5b030bef3f87f00cd0
emanuelefarelli <emaaliens888@gmail.com>
2023-06-27 17:28:23 +0200

    Added jumbotron content

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="jumbotron">
-        
-    </div>
+    <section class="jumbotron">
+        <div class="box">
+            <p class="banner">
+                welcome
+            </p>
+            <h1>
+                academic chess for <span>everyone</span>
+            </h1>
+            <p>
+                We are dedicated to broadening and developing chess as art, recreation, and as a significant element of culture in Northern California.
+            </p>
+            <div class="button">
+                <button class="enroll">
+                    Enroll Now
+                </button>
+                <button class="learn">
+                    Learn More
+                </button>
+            </div>
+        </div>
+    </section>
 </template>
     
 <script>
@@ -16,12 +34,62 @@
 </script>
     
 <style lang="scss" scoped>
-    div.jumbotron{
-        height: 80vh;
+@use '../styles/partials/mixins.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+    section.jumbotron{
+        height: 85vh;
         background-image: url(../assets/img/mt-2236-home-bg1.jpg);
         background-size: cover;
-        img{
-            width: 100%;
+        div.box{
+            @include marginAuto;
+            @include flex(column,center,flex-start);
+            height: 100%;
+            padding-right: 30%;
+            width: $dimension;
+            p.banner,
+            h1{
+                text-transform: uppercase;
+            }
+            p.banner{
+                font-size: $FontSizeXS;
+                font-weight: 300;
+                background-color: $yellowColor;
+                padding: 0.1rem 0.7rem;
+                border-radius: 1rem;
+                color: black;
+            }
+            h1{
+                font-size: $FontSizeXL;
+                font-weight: 600;
+                color: white;
+                span{
+                    color: $yellowColor;
+                    font-style: italic;
+                }
+            }
+            p{
+                color: white;
+                font-size: $FontSizeM;
+                padding: 2rem 0;
+            }
+            button{
+                padding: 1rem 3rem;
+                margin: 0.5rem;
+                border: none;
+                border-radius: 0.5rem;
+                font-size: $FontSizeM;
+            }
+            button.enroll{
+                background-color: $yellowColor;
+            }
+            button.learn{
+                background-color: rgba(0, 0, 0, 0);
+                color: white;
+                border: 3px solid white;
+            }
+        }
+        div.button{
+            @include flex();
         }
     }
 </style>
